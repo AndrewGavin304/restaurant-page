df38cfe3ac50bd7c54b84754e58a41ad5ae60551
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-29 15:09:40 -0400

    Actually setup workflow environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,13 @@
 const path = require('path');
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.resolve(__dirname, '.'),
-    hot: true
+    static: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
 };
