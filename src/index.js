3c886780d0a5d37779365a3d7f2acd3a93b0008d
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-30 17:16:51 -0400

    Homepage layout begun

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import createHomepage from './modules/homepage';
+import "./sass/_style.scss";
+
+
+createHomepage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
+// var result = sass.renderSync({
+//   file: scss_filename
+// })
+
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
@@ -9,5 +13,25 @@ module.exports = {
   },
   devServer: {
     static: './dist',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
