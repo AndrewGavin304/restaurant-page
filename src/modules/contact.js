feece58bbe6dd38fc3c2c10ee808f08ff5bcc501
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-08-03 09:57:03 -0400

    Make createCard.js; refactor contact.js to use it

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,15 +1,16 @@
 import { basicContainer } from "./basic-container";
+import { createCard } from "./createCard";
 import receptionImg from "../images/reception.jpg";
 import managerImg from "../images/manager.jpg";
 import chefImg from "../images/chef.jpg";
 
 class contactObject {
-  constructor(name, title, number, email, imgURL) {
+  constructor(name, title, number, email, img) {
     this.name = name;
     this.title = title;
     this.number = number,
     this.email = email,
-    this.imgURL = imgURL
+    this.img = img
   }
 }
 
@@ -17,41 +18,10 @@ let reception = new contactObject("Desk", "Reception", "1-334-359-0677", "Fiamat
 let manager = new contactObject("Luciana Scutari", "Manager", "1-669-985-3966", "ScutariWay@gmail.com", "../images/manager.jpg")
 let chef = new contactObject("Nico Lombardo", "Chef", "1-566-711-6236", "LombaroCreations@gmail.com", "../images/chef.jpg")
 
-const createContactCard = (object) => {
-  const cardContainer = document.createElement("div");
-  cardContainer.classList.add("card");
-
-  const title = document.createElement("span");
-  const name = document.createElement("span");
-  const number = document.createElement("span");
-  const email = document.createElement("span");
-  const image = document.createElement("img");
-
-  title.classList.add("card__title");
-  name.classList.add("card__name");
-  number.classList.add("card__number");
-  email.classList.add("card__email");
-  image.classList.add("card__img");
-
-  title.textContent = object.title;
-  name.textContent = object.name;
-  number.textContent = object.number;
-  email.textContent = object.email;
-  image.src = object.imgURL;
-
-  cardContainer.appendChild(title);
-  cardContainer.appendChild(name);
-  cardContainer.appendChild(number);
-  cardContainer.appendChild(email);
-  cardContainer.appendChild(image);
-
-  return cardContainer;
-}
-
 export const contact = () => {
   const contactContainer = basicContainer("contact");
 
-  contactContainer.appendChild(createContactCard(reception));
+  contactContainer.appendChild(createCard(reception, "contact-card"));
 
   return contactContainer;
 }
---
+++ src/modules/createCard.js
@@ -0,0 +1,23 @@
+export const createCard = (object, classname) => {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add(`${classname}`);
+
+  for (const [key, value] of Object.entries(object)) {
+
+    if (key == "img") {
+      const img = document.createElement("img");
+      img.classList.add(`${classname}__${key}`)
+      img.src = value;
+      cardContainer.appendChild(img)
+    }
+
+    else {
+      const span = document.createElement("span");
+      span.classList.add(`${classname}__${key}`);
+      span.textContent = value;
+      cardContainer.appendChild(span)
+    }
+  }
+  
+  return cardContainer;
+}
